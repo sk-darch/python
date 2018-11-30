c55ef741269449daa36f9e0bf2c1cd73f45f3c60
surmarp <38070724+surmarp@users.noreply.github.com>
2018-11-30 14:37:19 +0530

    Add files via upload

---
+++ getString.py
@@ -0,0 +1,8 @@
+#Get input from keyboard and print
+name = raw_input("What's your name?. : ")
+
+print("The Name entered by you is " + name )
+
+age = raw_input("how old are you ?. : ")
+
+print ("The age of Mr. " + name + " is " + age )
