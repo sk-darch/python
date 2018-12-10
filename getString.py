f4ab3310f96f9811a52c0b0c225651ee87c80043
surmarp <38070724+surmarp@users.noreply.github.com>
2018-12-10 15:19:28 +0530

    Update getString.py

--- getString.py
+++ getString.py
@@ -3,6 +3,6 @@ name = raw_input("What's your name?. : ")
 
 print("The Name entered by you is " + name )
 
-age = raw_input("how old are you ?. : ")
+age = int(raw_input("how old are you ?. : "))
 
 print ("The age of Mr. " + name + " is " + age )
