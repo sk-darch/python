0c6ae2c4405d2e1c59e6425682ea6465132cbde5
surmarp <38070724+surmarp@users.noreply.github.com>
2018-12-13 12:23:57 +0530

    Update inread.py

--- inread.py
+++ inread.py
@@ -7,7 +7,7 @@ fname = open("input.txt", 'r')
 for line in fname:
     words = len(line.split(" "))
     chars = len(line)
-    string += "Numaber of Words in line = %s\nNumber of Charactors in line = %s\n" % (words, chars)
+    string += "Number of Words in line = %s\nNumber of Characters in line = %s\n" % (words, chars)
 
 f = open("output.txt", "w+")
 f.write(string)
